7d71534642c30c226781ba184c9f867e11e6184e
Farook Jintha <farookjintha@gmail.com>
2020-03-16 15:36:01 +0530

    courses page templates added

--- Website/TechnicalCourses/models.py
+++ Website/TechnicalCourses/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Allcourses(models.Model):
     coursename = models.CharField(max_length=200)
     insname = models.CharField(max_length=100)
+    startedfrom = models.DateTimeField("Started from")
     def __str__(self):
         return self.coursename
+    
+    def was_published_recently(self):
+        return self.startedfrom >= timezone.now() - datetime.timedelta(days=1)
 
 class details(models.Model):
     course = models.ForeignKey(Allcourses, on_delete = models.CASCADE)
-    sp = models.CharField(max_length=500)
-    il = models.CharField(max_length=500)
+    ct = models.CharField(max_length=500)
+    your_choice = models.BooleanField(default = False)
+    # sp = models.CharField(max_length=500)
+    # il = models.CharField(max_length=500)
     def __str__(self):
-        return str(self.pk)
+        return str(self.ct)
