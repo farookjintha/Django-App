f19c04229a67926effc72f4692c1c32db352e8d7
Farook Jintha <farookjintha@gmail.com>
2020-03-16 19:00:10 +0530

    added choice func

--- Website/TechnicalCourses/urls.py
+++ Website/TechnicalCourses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'TechnicalCourses'
+
 urlpatterns = [
     path('<int:course_id>/',views.detail, name='detail'),
     path('', views.Courses, name = 'home-page'),
+    path('<int:course_id>/yourchoice/',views.your_choice, name = 'your-choice')
 ]
--- Website/TechnicalCourses/views.py
+++ Website/TechnicalCourses/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse 
 from .models import Allcourses
 from django.template import loader
 
@@ -13,8 +13,19 @@ def Courses(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, course_id):
+    course = get_object_or_404(Allcourses, pk = course_id)
+    return render(request, 'TechnicalCourses/detail.html',  {'course': course})
+
+def your_choice(request, course_id):
+    course = get_object_or_404(Allcourses, pk = course_id)
     try:
-        course = Allcourses.objects.get(pk=course_id)
-    except Allcourses.DoesNotExist:
-        raise Http404("Course Not Available")
-    return render(request, 'TechnicalCourses/detail.html', {'course': course})
+        selected_ct = course.details_set.get(pk = request.POST['choice'])
+    except (KeyError, Allcourses.DoesNotExist):
+        return render(request, 'TechnicalCourses/detail.html', {
+            'course': course,
+            'error_message': "Select valid option"
+        })
+    else:
+        selected_ct.your_choice = True
+        selected_ct.save()
+        return render(request, 'TechnicalCourses/detail.html', {'course': course})
